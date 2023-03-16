313da6bdc241e44cfd066c0dcbe6994e9db45b7f
sinif3-11 <sinif3-11@sinif3-11-iMac.local>
2023-03-16 16:50:08 +0300

    mui_lesson_added

--- src/routing/module/NavigationSource.jsx
+++ src/routing/module/NavigationSource.jsx
@@ -4,9 +4,9 @@ export default function NavigationSource(){
     return (
         <div className="bg-secondary">
        
-            <Link to= "/target" className="btn btn-warning m-1 w-25" >Hedefe Git</Link> <br />
+            <Link to= "/target" className="btn btn-warning m-3 w-25" >Hedefe Git</Link> <br />
             <Link to= "/target/301" className="btn btn-danger mb-3 w-25" >Ereğe Değişkenle Git</Link> <br />
-            <Link to= "/mui" className="btn btn-danger mb-3 w-25" >Mui Git</Link>
+            <Link to= "/mui" className="btn btn-danger mt-1 mb-3 w-25" >Mui Git</Link>
             
         </div>
     )
